12c43404ca31d784e57e99d018b96888032091dc
Satish Chandra Gupta <satishchandra538@gmail.com>
2020-05-29 15:14:08 +0530

    Line graph and donut chart moved from fetch to saperate function

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const totalDeathRateInDoc = document.getElementById("deathRate");
 const totalRecoveryRateInDoc = document.getElementById("recoveryRate");
 const worldTable = document.getElementById("worldTable");
 
+//Tool tip div element
+const div = d3
+    .select('body')
+    .append('div')
+    .attr('class', 'tooltip')
+
 const fetchData = async () => {
     const data = await fetch('https://pomber.github.io/covid19/timeseries.json');
     const jsonData = await data.json();
@@ -62,24 +68,152 @@ const fetchData = async () => {
     let totalRecoveryRate = Math.floor((totalRecovered * 100 / totalConfirmed) * 100) / 100;
     totalRecoveryRateInDoc.innerHTML = totalRecoveryRate + "%";
 
-    //Tool tip div element
-    const div = d3
-        .select('body')
-        .append('div')
-        .attr('class', 'tooltip')
-
     //Adding Donut char for whole wold data-----------------
-    
+    donutChart(totalActive,totalDeath);
+
+    //////////////////////////////////--------Adding Multiline Graph-----------
+    multiLineGraph(countries,days);
+
+    ///////////////----------Table making-----------
+    countries.forEach((country, index) => {
+        let TR = document.createElement("tr");
+        let TDcountry = document.createElement("td");
+        let TDcases = document.createElement("td");
+        let TDdeath = document.createElement("td");
+        let TDdeathrate = document.createElement("td");
+        let countryDeathRate = ((country[1][days - 1].deaths * 100) / country[1][days - 1].confirmed).toFixed(1);
+        if (countryDeathRate < 10) {
+            countryDeathRate = ". " + countryDeathRate;
+        }
+        let barWidth = (((country[1][days - 1].deaths / country[1][days - 1].confirmed) * 100) / countryWithHighestDeathRate) * 150;
+        let deathRateBar = `<svg width="100" height="20">
+            <rect width=${barWidth} height="20" fill="brown"></rect>
+        </svg>`
+        TDcountry.innerHTML = `${country[0]}`;
+        TDcases.innerHTML = country[1][days - 1].confirmed;
+        TDdeath.innerHTML = country[1][days - 1].deaths;
+        TDdeathrate.innerHTML = countryDeathRate + "% " + deathRateBar;
+        TR.append(TDcountry, TDcases, TDdeath, TDdeathrate)
+        worldTable.appendChild(TR);
+    })
+
+}
+fetchData()
+
+//---------MultiLine Graph------------------------/////////////
+const multiLineGraph = (countries, days) => {
+    var LineGraphParent = document.getElementById('worldMultilineChart').parentElement;
+    const lineGraphAxisMargin = { left: 60, top: 30, right: 80, bottom: 20 }
+    const svgLineGraph = d3.select('#worldMultilineChart')
+        .attr('width', LineGraphParent.offsetWidth)
+        .attr('height', LineGraphParent.offsetWidth * .4)
+
+    const gLineGraph = svgLineGraph.append('g')
+        .attr('transform', `translate(${60},${30})`)
+
+    const maxinfection = countries[0][1][days - 1].confirmed;
+    //Graph from day 51 to till date
+    const newData = [];
+    countries.forEach(country => {
+        let name = country[0];
+        let data = [];
+        for (let i = 50; i < days; i++) {
+            data.push(country[1][i]);
+        }
+        let set = [];
+        set.push(name);
+        set.push(data);
+        newData.push(set);
+    })
+
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(newData[0][1], d => d.date))
+        //.domain([countries[0][1][50].date, countries[0][1][days-1].date])
+        .range([0, LineGraphParent.offsetWidth])
+        .nice()
+    const xTicks = 6;
+    const xAxis = d3.axisBottom(xScale)
+        .ticks(xTicks)
+    //.tickSize(-LineGraphParent.offsetHeight + 65);
+    gLineGraph.append('g')
+        .call(xAxis)
+        .attr('class', 'xaxis')
+        .attr('transform', `translate(${0},${LineGraphParent.offsetWidth * .4 - lineGraphAxisMargin.bottom - 30})`);
+
+    const yScale = d3.scaleLinear()
+        .domain([maxinfection, 0])
+        .range([0, LineGraphParent.offsetWidth * .4 - 30])
+        .nice()
+    const yAxis = d3.axisLeft(yScale)
+    //.tickSize(-LineGraphParent.offsetWidth + 70);;
+    gLineGraph.append('g')
+        .call(yAxis)
+        .attr('class', 'yaxis')
+        .attr('transform', `translate(${0},${-lineGraphAxisMargin.bottom})`)
+    const lineColor = d3.scaleOrdinal().range(d3.schemeCategory10);
+    const line = d3.line()
+        .curve(d3.curveMonotoneX)
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.confirmed));
+
+    const top10 = [];
+    for (let i = 0; i < 10; i++) {
+        top10.push(newData[i]);
+    }
+
+    var country = gLineGraph.selectAll(".country")
+        .data(top10)
+        .enter()
+        .append("g")
+        .attr("class", d => `country ${d[0]}`);
+
+    country.append('path')
+        .attr('fill', 'none')
+        .style("stroke", d => lineColor(d[0]))
+        .attr("stroke-width", 1.5)
+        .attr("d", d => line(d[1]))
+        .attr('transform', `translate(${-60},${-lineGraphAxisMargin.bottom})`)
+        .on('mouseover', d => {
+            //console.log(d3.event, xScale(-d3.event.layerX*100))
+            div
+                .transition()
+                .duration(200)
+                .style('opacity', 0.9);
+            div.html(
+                `<span style="color:${lineColor(d[0])};font-size:16px;font-weight:bold">${d[0]}</span>`
+                + '<br/>'
+                + `<span style="color:${lineColor(d[0])};">total cases=${d[1][days - 1 - 50].confirmed}</span>`)
+                .style('left', d3.event.pageX + 'px')
+                .style('top', d3.event.pageY - 28 + 'px');
+        })
+    // .on('mouseout', () => {
+    //     div
+    //         .transition()
+    //         .duration(500)
+    //         .style('opacity', 0);
+    // })
+    country.append('text')
+        .style("fill", d => lineColor(d[0]))
+        .text(d => d[0])
+        .style("font-weight", 600)
+        .attr('x', d => xScale(d[1][days - 1 - 50].date) - 90)
+        .attr('y', d => yScale(d[1][days - 1 - 50].confirmed) - 20)
+        .attr('class', 'line-graph-country-legend')
+    //.attr('translate',`transform(${0},${0})`)
+}
+
+//-----------Donut Chart---------------////////////
+const donutChart=(totalActive,totalDeath)=>{
     var pieCharParent = document.getElementById('worldActivePieChart').parentElement;
     var pieChartWidth = pieCharParent.offsetWidth * 0.6;
     var radius = pieChartWidth / 2;
     const donutData = [{ label: 'totalRecovered', value: totalRecovered },
-                       { label: 'totalActive', value: totalActive },
-                       { label: 'totalDeath', value: totalDeath }];
+    { label: 'totalActive', value: totalActive },
+    { label: 'totalDeath', value: totalDeath }];
 
     var worldActivePieChart = d3.select("#worldActivePieChart")
         .attr("width", pieCharParent.offsetWidth)
-        .attr("height", pieCharParent.offsetWidth/1.2)
+        .attr("height", pieCharParent.offsetWidth / 1.2)
         .append('g')
         .attr('transform', `translate(${pieChartWidth / 1.2},${pieChartWidth / 1.2})`)
 
@@ -147,132 +281,8 @@ const fetchData = async () => {
             var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
             return (midangle < Math.PI ? 'start' : 'end')
         })
-
-    //////////////////////////////////--------Adding Multiline Graph-----------
-    var LineGraphParent = document.getElementById('worldMultilineChart').parentElement;
-
-    const svgLineGraph = d3.select('#worldMultilineChart')
-        .attr('width', LineGraphParent.offsetWidth)
-        .attr('height', LineGraphParent.offsetWidth * .4)
-
-    const gLineGraph = svgLineGraph.append('g')
-        .attr('transform', `translate(${60},${30})`)
-
-    const maxinfection = countries[0][1][countries[0][1].length - 1].confirmed;
-
-    //Graph from day 51 to till date
-    const newData = [];
-    countries.forEach(country => {
-        let name = country[0];
-        let data = [];
-        for (let i = 50; i < days; i++) {
-            data.push(country[1][i]);
-        }
-        let set = [];
-        set.push(name);
-        set.push(data);
-        newData.push(set);
-    })
-
-    const xScale = d3.scaleTime()
-        .domain(d3.extent(newData[0][1], d => d.date))
-        //.domain([countries[0][1][50].date, countries[0][1][days-1].date])
-        .range([0, LineGraphParent.offsetWidth])
-        .nice()
-    const xTicks = 6;
-    const xAxis = d3.axisBottom(xScale)
-        .ticks(xTicks)
-        .tickSize(-LineGraphParent.offsetHeight + 65);
-    gLineGraph.append('g')
-        .call(xAxis)
-        .attr('class', 'xaxis')
-        .attr('transform', `translate(${0},${LineGraphParent.offsetWidth * .4 - 55})`);
-
-    const yScale = d3.scaleLinear()
-        .domain([maxinfection, 0])
-        .range([0, LineGraphParent.offsetWidth * .4 - 30])
-        .nice()
-    const yAxis = d3.axisLeft(yScale)
-        .tickSize(-LineGraphParent.offsetWidth + 70);;
-    gLineGraph.append('g')
-        .call(yAxis)
-        .attr('class', 'xaxis')
-        .attr('transform', `translate(${0},${-25})`)
-    const lineColor = d3.scaleOrdinal().range(d3.schemeCategory10);
-    const line = d3.line()
-        .curve(d3.curveMonotoneX)
-        .x(d => xScale(d.date))
-        .y(d => yScale(d.confirmed));
-
-    const top10 = [];
-    for (let i = 0; i < 10; i++) {
-        top10.push(newData[i]);
-    }
-
-    var country = gLineGraph.selectAll(".country")
-        .data(top10)
-        .enter()
-        .append("g")
-        .attr("class", d => `country ${d[0]}`);
-
-    country.append('path')
-        .attr('fill', 'none')
-        .style("stroke", d => lineColor(d[0]))
-        .attr("stroke-width", 1.5)
-        .attr("d", (d, i) => line(d[1]))
-        .attr('transform', `translate(${-60},${-25})`)
-        .on('mouseover', d => {
-            //console.log(d3.event, xScale(-d3.event.layerX*100))
-            div
-                .transition()
-                .duration(200)
-                .style('opacity', 0.9);
-            div.html(
-                `<span style="color:${lineColor(d[0])};font-size:16px;font-weight:bold">${d[0]}</span>`
-                + '<br/>'
-                + `<span style="color:${lineColor(d[0])};">total cases=${d[1][days - 1 - 50].confirmed}</span>`)
-                .style('left', d3.event.pageX + 'px')
-                .style('top', d3.event.pageY - 28 + 'px');
-        })
-        // .on('mouseout', () => {
-        //     div
-        //         .transition()
-        //         .duration(500)
-        //         .style('opacity', 0);
-        // })
-    country.append('text')
-        .style("fill", d => lineColor(d[0]))
-        .text(d => d[0])
-        .style("font-weight", 600)
-        .attr('x', d => xScale(d[1][days - 1 - 50].date) - 60)
-        .attr('y', d => yScale(d[1][days - 1 - 50].confirmed) - 20)
-    //.attr('translate',`transform(${0},${0})`)
-
-    ///////////////----------Table making-----------
-    countries.forEach((country, index) => {
-        let TR = document.createElement("tr");
-        let TDcountry = document.createElement("td");
-        let TDcases = document.createElement("td");
-        let TDdeath = document.createElement("td");
-        let TDdeathrate = document.createElement("td");
-        let countryDeathRate = ((country[1][days - 1].deaths * 100) / country[1][days - 1].confirmed).toFixed(1);
-        if (countryDeathRate < 10) {
-            countryDeathRate = ". " + countryDeathRate;
-        }
-        let barWidth = (((country[1][days - 1].deaths / country[1][days - 1].confirmed) * 100) / countryWithHighestDeathRate) * 150;
-        let deathRateBar = `<svg width="100" height="20">
-            <rect width=${barWidth} height="20" fill="brown"></rect>
-        </svg>`
-        TDcountry.innerHTML = `${country[0]}`;
-        TDcases.innerHTML = country[1][days - 1].confirmed;
-        TDdeath.innerHTML = country[1][days - 1].deaths;
-        TDdeathrate.innerHTML = countryDeathRate + "% " + deathRateBar;
-        TR.append(TDcountry, TDcases, TDdeath, TDdeathrate)
-        worldTable.appendChild(TR);
-    })
-
 }
-fetchData()
+
 
 ///////////filter in table
 const searchCountry = () => {
